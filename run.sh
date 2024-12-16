41c43976b27110025546a3d46d3a0ee51b61ea97
mystic76 <contact.iamlynx@gmail.com>
2024-12-16 11:03:38 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-!lscpu
+lscpu
 sleep 5
-!clear
+clear
 !echo "Starting Roop-Unleashed"
 sleep 2
-!echo "installing packages from"
-!echo "github.com/abhiram79/"
+echo "installing packages from"
+echo "github.com/abhiram79/"
 sleep 5
-!apt-get -y update
-!apt-get -y install cuda-toolkit-11-8
+apt-get -y update
+apt-get -y install cuda-toolkit-11-8
 import os
 os.environ["LD_LIBRARY_PATH"] += ":" + "/usr/local/cuda-11/lib64"
 os.environ["LD_LIBRARY_PATH"] += ":" + "/usr/local/cuda-11.8/lib64"
-!git clone https://github.com/C0untFloyd/roop-unleashed.git
+git clone https://github.com/C0untFloyd/roop-unleashed.git
 %cd roop-unleashed
-!mv config_colab.yaml config.yaml
-!pip install -r requirements.txt
-!clear
-!echo "Connecting to GUI"
+mv config_colab.yaml config.yaml
+pip install -r requirements.txt
+clear
+echo "Connecting to GUI"
 sleep 4
-!clear
-!echo "Run the second cell now!"
+clear
+echo "Run the second cell now!"
 #end
 
